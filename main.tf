ac7eb4a0bbb82e326389cdaeffcfd108cce24109
sunitadubey21 <90917307+sunitadubey21@users.noreply.github.com>
2023-07-05 19:03:18 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,5 @@
-#module "cencosud_cognito" {
-#source = "./modules/redshift-query/cognito"
-#}
-
-#module "util_layer" {
-#  source = "./util-layer"
-#}
-
 module "redshift_layer" {
   source = "./modules/redshift-query"
 
   vpc_id     = var.vpc_id
 }
-
-#module "cencosud_api" {
-#  source                 = "../modules/redshift-query/api-gateway"
-#  cognito_user_arn       = module.cencosud_cognito.cencosud_cognito_user_pool_arn
-#  api_status_response    = ["200", "500"]
-#  aws_region             = var.aws_region
-#  account_id             = var.account_id
-#  util_layer_arn_array   = module.util_layer.util_layer_arn_array
-#}
