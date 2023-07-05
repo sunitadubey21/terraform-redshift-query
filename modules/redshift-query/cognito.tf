3f9a32f0f3803caec75d60b62ee1ea103ad83e12
sunitadubey21 <90917307+sunitadubey21@users.noreply.github.com>
2023-07-05 19:10:47 -0400

    Update cognito.tf

--- modules/redshift-query/cognito.tf
+++ modules/redshift-query/cognito.tf
@@ -22,19 +22,6 @@ resource "aws_cognito_user_pool" "rest_api_user_pool" {
     attribute_data_type      = "String"
     developer_only_attribute = false
     mutable                  = true
-    name                     = "email"
-    required                 = true
-
-    string_attribute_constraints {
-      min_length = 7
-      max_length = 256
-    }
-  }
-
-  schema {
-    attribute_data_type      = "String"
-    developer_only_attribute = false
-    mutable                  = true
     name                     = "name"
     required                 = true
 
@@ -47,10 +34,54 @@ resource "aws_cognito_user_pool" "rest_api_user_pool" {
 
 resource "aws_cognito_user_pool_client" "rest_api_user_pool_client" {
   name                = "cencosud-api-user-pool-client"
+  callback_urls = var.identity_provider_callback_urls
   explicit_auth_flows = [
     "ALLOW_USER_PASSWORD_AUTH",
     "ALLOW_REFRESH_TOKEN_AUTH"
   ]
+  allowed_oauth_flows                  = ["code"]
+  allowed_oauth_flows_user_pool_client = true
+  allowed_oauth_scopes = [
+    "email",
+    "openid",
+    "phone","profile","aws.cognito.signin.user.admin"
+]
+    user_pool_id = aws_cognito_user_pool.rest_api_user_pool.id
+}
 
+// Create an App client
+resource "aws_cognito_user_pool_client" "client" {
+  name         = "cencosudtestuser_client"
   user_pool_id = aws_cognito_user_pool.rest_api_user_pool.id
+  // We can configure additional providers like Facebook, Google, etc
+  supported_identity_providers = [
+    "COGNITO",
+  ]
+  callback_urls                        = var.identity_provider_callback_urls
+  allowed_oauth_flows                  = ["code"]
+  allowed_oauth_flows_user_pool_client = true
+  explicit_auth_flows = [
+    "ALLOW_USER_PASSWORD_AUTH",
+    "ALLOW_REFRESH_TOKEN_AUTH"
+  ]
+  allowed_oauth_scopes = [
+    "email",
+    "openid",
+    "phone","profile","aws.cognito.signin.user.admin"
+  ]
+}
+
+resource "aws_cognito_user_pool_domain" "main" {
+  user_pool_id = aws_cognito_user_pool.rest_api_user_pool.id
+  domain       = var.cognito_user_pool_domain
+}
+
+resource "aws_cognito_user" "example" {
+  user_pool_id = aws_cognito_user_pool.rest_api_user_pool.id
+  username     = "cognitotestuser2"
+  password     = "Test123456*"
+  attributes = {
+    email          = "no-reply@hashicorp.com"
+    email_verified = true
+  }
 }
